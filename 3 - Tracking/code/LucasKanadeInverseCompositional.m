f9a972a2bdf7f8520488848552ab8a07f4b5389e
Wang Yang <wangyang.evan@qq.com>
2017-11-23 17:51:12 -0800

    finish lucas kanade naive version without template drifting correction

--- 3 - Tracking/code/LucasKanadeInverseCompositional.m
+++ 3 - Tracking/code/LucasKanadeInverseCompositional.m
@@ -2,11 +2,15 @@ function [u,v] = LucasKanadeInverseCompositional(It, It1, rect)
 
 % input - image at time t, image at t+1, rectangle (top left, bot right coordinates)
 % output - movement vector, [u,v] in the x- and y-directions.
-    T = It(rect(2):rect(4),rect(1):rect(3));
+    M = round(rect(4) - rect(2));
+    N = round(rect(3) - rect(1));
+    
+    Xq0 = repmat([1:M]' + rect(2), 1, N);
+    Yq0 = repmat([1:N] + rect(1), M, 1);
+    T = interp2(im2double(It), Yq0, Xq0);
     %I = It1;
     
     [GTx, GTy] = imgradientxy(T);
-    [M,N] = size(T);
     
     SDI = zeros(M,N,2);
     SDI(:,:,1) = ones(M,N)*0.5 .* GTy;
@@ -23,7 +27,6 @@ function [u,v] = LucasKanadeInverseCompositional(It, It1, rect)
     
     p = [0;0];
     
-    
     for ii = 1:12000
         %%%%%%%
         %Xq = repmat([1:M]' + (p(1)+rect(1)), 1, N);
@@ -32,9 +35,18 @@ function [u,v] = LucasKanadeInverseCompositional(It, It1, rect)
         Yq= repmat([1:N] + (p(2)+rect(1)), M, 1);
         I = interp2(im2double(It1), Yq, Xq);
         
-        imshow(I)
+%         IMG = It1;
+%         for iii = round(p(1)+rect(2)) : round(p(1)+rect(2)+M)
+%             IMG(iii, round(p(2)+rect(1))) = 255;
+%             IMG(iii, round(p(2)+rect(1)+N)) = 255;
+%         end
+%         for jjj = round(p(2)+rect(1)): round(p(2)+rect(1)+N)
+%             IMG(round(p(1)+rect(2)), jjj) = 255;
+%             IMG(round(p(1)+rect(2)+M), jjj) = 255;
+%         end
+%         imshow(IMG)
 
-        ErrorImage = I - im2double(T);
+        ErrorImage = I - T;
         %imshow(ErrorImage*10+0.5);
 
         SD = [0;0];
@@ -44,10 +56,16 @@ function [u,v] = LucasKanadeInverseCompositional(It, It1, rect)
 
         dp = InvH * SD;
 
-        p = p - dp*1000;
+        p = p - dp;
         
-        [dp, p, [ii;0]]
+        if (abs(dp(1)) < 0.01 && abs(dp(2)) < 0.01 )
+            break
+        end
     end
+    
+    u = p(1);
+    v = p(2);
+    return
     
 end
 
