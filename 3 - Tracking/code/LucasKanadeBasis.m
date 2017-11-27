582e896aaf8e5f5d3fb96c5cd2eb196ffce384db
Wang Yang <wangyang.evan@qq.com>
2017-11-26 18:43:46 -0800

    add bases

--- 3 - Tracking/code/LucasKanadeBasis.m
+++ 3 - Tracking/code/LucasKanadeBasis.m
@@ -3,3 +3,74 @@ function [u,v] = LucasKanadeBasis(It, It1, rect, bases)
 % input - image at time t, image at t+1, rectangle (top left, bot right
 % coordinates), bases 
 % output - movement vector, [u,v] in the x- and y-directions.
+    M = round(rect(4) - rect(2));
+    N = round(rect(3) - rect(1));
+    
+    Xq0 = repmat([1:M]' + rect(2), 1, N);
+    Yq0 = repmat([1:N] + rect(1), M, 1);
+    T = interp2(im2double(It), Yq0, Xq0);
+    %I = It1;
+    
+    [GTx, GTy] = imgradientxy(T);
+    
+    SDI0 = zeros(M,N,2);
+    SDI0(:,:,1) = ones(M,N)*0.5 .* GTy;
+    SDI0(:,:,2) = ones(M,N)*0.5 .* GTx;
+    
+    SDQ = zeros(M,N,2);
+    basisSize = size(bases);
+    basisCount = basisSize(3);
+    tempSum = zeros(M,N,2);
+    
+    tmp = zeros(basisCount,2);
+    for i = 1:basisCount
+        tmp(i,:) = [sum(sum(bases(:,:,i).*SDI0(:,:,1))), sum(sum(bases(:,:,i).*SDI0(:,:,2)))];
+    end
+    for m = 1:M
+        for n=1:N
+            for i = 1:basisCount
+                tmp2 = tmp(i,:) * bases(m,n,i);
+                tempSum(m,n,:) = tempSum(m,n,:) + reshape(tmp2,1,1,2);
+            end
+        end
+    end
+    SDQ = SDI0 - tempSum;
+    
+    H = zeros(2,2);
+    for i = 1:2
+        for j = 1:2
+            H(i,j) = sum(sum(SDQ(:,:,i).*SDQ(:,:,j)));
+        end
+    end
+    
+    InvH = inv(H);
+    
+    p = [0;0];
+    
+    for ii = 1:12000
+        Xq = repmat([1:M]' + (p(1)+rect(2)), 1, N);
+        Yq= repmat([1:N] + (p(2)+rect(1)), M, 1);
+        I = interp2(im2double(It1), Yq, Xq);
+
+        ErrorImage = I - T;
+
+        SD = [0;0];
+        for i = 1:2
+            SD(i) = sum(sum(SDQ(:,:,i) .* ErrorImage));
+        end
+
+        dp = InvH * SD;
+
+        p = p - dp;
+        
+        if (abs(dp(1)) < 0.01 && abs(dp(2)) < 0.01 )
+            ii
+            break
+        end
+    end
+    
+    u = p(1);
+    v = p(2);
+    return
+    
+end
--- 3 - Tracking/code/testSylvSequence.m
+++ 3 - Tracking/code/testSylvSequence.m
@@ -1,18 +1,47 @@
 % load frames
-
+load('data/sylvbases.mat')
+load('data/sylvseq.mat')
 
 % implementation
+frameSize = size(frames);
+frameCount = frameSize(3);
 
+rect = [102-1, 62-1, 156, 108];
+rects = [];
+rect2 = [102-1, 62-1, 156, 108];
 
-
-
-
-
-
-
-
-
-
+for i = 2 : frameCount
+    [u,v] = LucasKanadeBasis(frames(:,:,i-1), frames(:,:,i), rect, bases);
+    [u2,v2] = LucasKanadeInverseCompositional(frames(:,:,i-1), frames(:,:,i), rect2);
+    rect = rect + [v,u,v,u];
+    rect2 = rect2 + [v2,u2,v2,u2];
+    rects = [rects;rect];
+    %imshow(frames(:,:,i))
+    %rectangle('Position',[rect(1), rect(2), rect(3)-rect(1), rect(4)-rect(2)])
+     IMG = repmat(frames(:,:,i), 1, 1, 3);
+     
+     for iii = round(rect(2)) : round(rect(4))
+         IMG(iii, round(rect(1)), :) = [0,255,0];
+         IMG(iii, round(rect(3)), :) = [0,255,0];
+     end
+     for jjj = round(rect(1)): round(rect(3))
+         IMG(round(rect(2)), jjj, :) = [0,255,0];
+         IMG(round(rect(4)), jjj, :) = [0,255,0];
+     end
+     
+     
+     for iii = round(rect2(2)) : round(rect2(4))
+         IMG(iii, round(rect2(1)), :) = [255,255,0];
+         IMG(iii, round(rect2(3)), :) = [255,255,0];
+     end
+     for jjj = round(rect2(1)): round(rect2(3))
+         IMG(round(rect2(2)), jjj, :) = [255,255,0];
+         IMG(round(rect2(4)), jjj, :) = [255,255,0];
+     end
+     
+     
+     imshow(IMG)
+end
 
 
 
